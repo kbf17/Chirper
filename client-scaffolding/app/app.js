b61a674a73235d2d64781fd9e81f284eebb3811a
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-07-18 15:02:22 -0500

    gets chirps

--- client-scaffolding/app/app.js
+++ client-scaffolding/app/app.js
@@ -1,18 +1,59 @@
 (function(){
-    var app = angular.module('myApp', []);
+    var app = angular.module('myApp', ["ngRoute",]);
 
-    app.controller('CheepController', function(){
-        this.chirp = cheep
-    })
+    app.config(function($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl : "../views/home.html"
+        })
+        .when("/list", {
+            templateUrl : "../views/list.html",
+            controller: "ChirpsController"
+        })
+        .when("/single", {
+            templateUrl : "../views/single.html",
 
-    var cheep = [
-        {
-        body: "hello world",
-        name: "Kristine",
-        id: ""
-        },
-    ]
+        })
+    });
 
+
+    app.controller('ChirpsController', function($scope, $http) {
+        $http.get("http://localhost:3000/api/chirps")
+        .then(function(response){
+            $scope.chirpList = response.data;
+            console.log('found');
+            console.log($scope.chirpList);   
+        })
+        // .error(function(response){
+        //     confirm("Uh oh! Cheeps not found!" + "Refresh stream?");
+        // })
+    });
+
+    // app.controller('SingleController', function($scope, $http){
+    //     $http.get("http://localhost:3000/api/chirps/:" + id)
+    //     .then(function(response){
+    //         $scope.singleChirp = response.data;
+    //         console.log('single');
+    //     })
+    // })
+
+    // app.controller('PushController', function($scope, $http){
+    //     $scope.SendChirp = function(){
+    //         var data = $.param({
+    //             user : $scope.user,
+    //             message : $scope.message
+    //         });
+    //         var config = {
+    //             headers : {
+    //                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+    //             }
+    //         }
+    //         console.log(data);
+    //         $http.post("http://localhost:3000/api/chirps", data, config)
+    //         .success('Posted')
+    //         .error('failure');
+    //     }
+    // });
 
 
 
